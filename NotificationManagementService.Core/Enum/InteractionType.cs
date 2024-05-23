df182bd54c9fbd120c97b637d7dd43fd5e742aca
rastilean <raresastilean@yahoo.com>
2024-05-23 20:43:49 +0300

    Refactor

---
+++ NotificationManagementService.Core/Enum/InteractionType.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationManagementService.Core.Enum
+{
+    public enum InteractionType
+    {
+        VIEW = 0,
+        LIKE = 1
+    }
+}
---
+++ NotificationManagementService.Core/Model/Interaction.cs
@@ -0,0 +1,13 @@
+﻿using NotificationManagementService.Core.Enum;
+
+namespace NotificationManagementService.Core.Model
+{
+    public class Interaction
+    {
+        public string UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public InteractionType InteractionType { get; set; }
+    }
+}
